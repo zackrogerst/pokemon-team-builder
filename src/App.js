80921fdfd90c297b6819789f8364a10e8ed0d17d
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-03 10:57:46 -0600

    added useEffect axios call

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import "./App.css";
 import Header from "./components/Header";
 import TeamSection from "./components/TeamSection";
 import PokedexSection from "./components/PokedexSection";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+	const [allPokemon, setAllPokemon] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("https://pokeapi.co/api/v2/pokemon?limit=100000")
+			.then(res => setAllPokemon(res.data.results))
+			.catch(err => console.log(err));
+	}, []);
+
 	return (
 		<div className="App">
 			<Header />

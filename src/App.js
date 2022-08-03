2e9855995f0ace534648bba0041ab753d1ac488d
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-03 10:30:45 -0600

    added TeamSection and cards. Plus styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import TeamSection from "./components/TeamSection";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
+			<TeamSection />
 		</div>
 	);
 }
---
+++ src/components/TeamSection.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const TeamSection = () => {
+	return (
+		<section className="team-section">
+			<h2>My Team</h2>
+			<div className="team-container">
+				<article className="team-card">
+					<h3>Pokemon Card</h3>
+				</article>
+                <article className="team-card">
+					<h3>Pokemon Card</h3>
+				</article>
+                <article className="team-card">
+					<h3>Pokemon Card</h3>
+				</article>
+                <article className="team-card">
+					<h3>Pokemon Card</h3>
+				</article>
+                <article className="team-card">
+					<h3>Pokemon Card</h3>
+				</article>
+                <article className="team-card">
+					<h3>Pokemon Card</h3>
+				</article>
+			</div>
+		</section>
+	);
+};
+
+export default TeamSection;

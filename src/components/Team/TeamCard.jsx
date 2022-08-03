b34e7c1296f43ee1ca9206a981ef02f8fd5e119b
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-03 12:31:26 -0600

    updated TeamCard.jsx

--- src/components/Team/TeamCard.jsx
+++ src/components/Team/TeamCard.jsx
@@ -3,10 +3,8 @@ import React from "react";
 const TeamCard = ({pokemon}) => {
 	return (
 		<article className="team-card">
-            {console.log(pokemon)}
 			<h3>{pokemon.name}</h3>
-            {console.table(pokemon)}
-			{/* <p>{JSON.stringify(pokemon)}</p> */}
+			<p>{JSON.stringify(pokemon.stats)}</p>
 		</article>
 	);
 };
